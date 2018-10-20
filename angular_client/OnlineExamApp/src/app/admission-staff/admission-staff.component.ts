17bf6e34043b77b53daf5ebf8350a3ecc21cd98c
melathadgu <abemel26@gmail.com>
2018-10-20 11:42:22 -0500

    admission component created

---
+++ angular_client/OnlineExamApp/src/app/admission-staff/admission-staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-admission-staff',
+  templateUrl: './admission-staff.component.html',
+  styleUrls: ['./admission-staff.component.css']
+})
+export class AdmissionStaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
