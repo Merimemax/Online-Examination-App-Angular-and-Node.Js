a90bdfdae98b74d7ca8050c646ccef3b439ab382
Merih <merimemax@gmail.com>
2018-10-23 18:18:15 -0500

    minor updates

--- nodejs_server/myapp/app.js
+++ nodejs_server/myapp/app.js
@@ -24,14 +24,14 @@ var routesApi = require('./api/routes/index');
 //......configuring cors
 var corsOptions = {
   origin: 'http://localhost:4200',
-  credentials:true,
-  methods:['GET', 'PUT', 'POST'],
-  allowedHeaders:'Content-Type',
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST'],
+  allowedHeaders: 'Content-Type',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-
 }
 var app = express();
 app.use(cors(corsOptions));
+
 //....
 
 
